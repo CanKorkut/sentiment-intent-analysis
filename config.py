015dcd40390f5278a95bdfe0e5ecf31fce201e48
Can Korkut <33387838+CanKorkut@users.noreply.github.com>
2025-05-11 11:47:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY", None)
     GROQ_MODEL_NAME = "llama3-8b-8192"
     SENTIMENT_MODEL_NAME = "cardiffnlp/twitter-roberta-base-sentiment"
 
